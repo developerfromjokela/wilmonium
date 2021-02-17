c3b1f5cef3db0da494dc9f0d6fa95b8848d1adf7
ahnl <46541386+ahnl@users.noreply.github.com>
2021-02-17 15:15:05 +0200

    Add light/dark theme toggle switch
    Stolen from https://codepen.io/bheberer/pen/BaNZKmq

--- js/loader.js
+++ js/loader.js
@@ -5,6 +5,37 @@ const wilmoniumDebug = (msg) => {
         console.log("[wilmonium] "+msg);
 }
 
+const addStyles = (filename, id) => {
+    var link = document.createElement('link');
+    link.href = chrome.extension.getURL(filename);
+    link.type = 'text/css';
+    link.rel = 'stylesheet';
+    link.media = 'all';
+    if (id) link.id = id;
+    document.getElementsByTagName('HEAD')[0].appendChild(link);
+}
+const addHtml = (html) => {
+    document.body.insertAdjacentHTML('beforeend', html);
+}
+
+const loadJS = (url) => {
+    var scriptTag = document.createElement('script');
+    scriptTag.src = url;
+
+    document.body.appendChild(scriptTag);
+};
+
+Element.prototype.remove = function() {
+    this.parentElement.removeChild(this);
+}
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+    for(var i = this.length - 1; i >= 0; i--) {
+        if(this[i] && this[i].parentElement) {
+            this[i].parentElement.removeChild(this[i]);
+        }
+    }
+}
+
 // Inject code to wilma page, if it happens to be logged in
 if (document.getElementById("logout-form")) {
     let script = document.createElement('script');
@@ -13,12 +44,41 @@ if (document.getElementById("logout-form")) {
     wilmoniumDebug("Injecting "+chrome.extension.getURL('js/wilmonium.js'));
     document.body.appendChild(script);
 
-    var link = document.createElement('link');
-    link.href = chrome.extension.getURL('css/wilmonium-dark.css');
-    link.type = 'text/css';
-    link.rel = 'stylesheet';
-    link.media = 'all';
-    document.getElementsByTagName('HEAD')[0].appendChild(link);
+    let darkmode = JSON.parse(localStorage.getItem('wilmonium')).darkmode;
+
+    if (darkmode) addStyles('css/wilmonium-dark.css', 'darkmode-css');
+
+    
+    /* Toggle switch */
+    addStyles('css/wilmonium.css');
+    loadJS('https://code.iconify.design/1/1.0.4/iconify.min.js');
+    addHtml(`
+    <label>
+    <input class='toggle-checkbox' type='checkbox' ${darkmode ? 'checked' : ''} />
+    <div class='toggle-slot'>
+        <div class='sun-icon-wrapper'>
+        <div class="iconify sun-icon" data-icon="feather-sun" data-inline="false"></div>
+        </div>
+        <div class='toggle-button'></div>
+        <div class='moon-icon-wrapper'>
+        <div class="iconify moon-icon" data-icon="feather-moon" data-inline="false"></div>
+        </div>
+    </div>
+    </label>
+    `);
+    document.querySelector('.toggle-checkbox').onchange = (evt) => {
+        let darkmode = evt.target.checked;
+        wilmoniumDebug('Dark mode ' + darkmode)
+        if (!darkmode) {
+            document.querySelectorAll('#darkmode-css').forEach(el => {
+                el.remove();
+            })
+            
+        } else {
+            addStyles('css/wilmonium-dark.css', 'darkmode-css');
+        }
+        localStorage.setItem('wilmonium', JSON.stringify({darkmode: darkmode}));
+    }
     //document.getElementsByTagName("footer")[0].children[0].src = "https://media.discordapp.net/attachments/798799175072219136/798840488819294238/treegif.gif";
 }
 
