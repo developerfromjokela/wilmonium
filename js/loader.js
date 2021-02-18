b66c9199d10d5b0a05e475653f5e4dbe8f11a129
developerfromjokela <developerfromjokela@gmail.com>
2021-02-18 12:18:32 +0200

    Fixed dark theme in course tray

--- js/loader.js
+++ js/loader.js
@@ -36,26 +36,29 @@ NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
     }
 }
 
-// Inject code to wilma page, if it happens to be logged in
-if (document.getElementById("logout-form")) {
-    let script = document.createElement('script');
-    script.setAttribute('type', 'text/javascript');
-    script.setAttribute('src', chrome.extension.getURL('js/wilmonium.js'));
-    wilmoniumDebug("Injecting "+chrome.extension.getURL('js/wilmonium.js'));
-    document.body.appendChild(script);
+try {
+    let navDetect = document.getElementsByClassName("nav-wilma").length > 0;
+    let selectionDetect = document.getElementsByClassName("selection-body").length > 0;
+    // Inject code to wilma page, if it happens to be logged in
+    if ( navDetect || selectionDetect) {
+        let script = document.createElement('script');
+        script.setAttribute('type', 'text/javascript');
+        script.setAttribute('src', chrome.extension.getURL('js/wilmonium.js'));
+        wilmoniumDebug("Injecting "+chrome.extension.getURL('js/wilmonium.js'));
+        document.body.appendChild(script);
 
-    if (localStorage.getItem("wilmonium") == null) {
-        localStorage.setItem("wilmonium", JSON.stringify({darkmode: false}));
-    }
-    let darkmode = JSON.parse(localStorage.getItem('wilmonium')).darkmode;
+        if (localStorage.getItem("wilmonium") == null) {
+            localStorage.setItem("wilmonium", JSON.stringify({darkmode: false}));
+        }
+        let darkmode = JSON.parse(localStorage.getItem('wilmonium')).darkmode;
 
-    if (darkmode) addStyles('css/wilmonium-dark.css', 'darkmode-css');
+        if (darkmode) addStyles('css/wilmonium-dark.css', 'darkmode-css');
 
-    
-    /* Toggle switch */
-    addStyles('css/wilmonium.css');
-    loadJS('https://code.iconify.design/1/1.0.4/iconify.min.js');
-    addHtml(`
+
+        /* Toggle switch */
+        addStyles('css/wilmonium.css');
+        loadJS('https://code.iconify.design/1/1.0.4/iconify.min.js');
+        addHtml(`
     <label>
     <input class='toggle-checkbox' type='checkbox' ${darkmode ? 'checked' : ''} />
     <div class='toggle-slot'>
@@ -69,29 +72,32 @@ if (document.getElementById("logout-form")) {
     </div>
     </label>
     `);
-    document.querySelector('.toggle-checkbox').onchange = (evt) => {
-        let darkmode = evt.target.checked;
-        wilmoniumDebug('Dark mode ' + darkmode)
-        if (!darkmode) {
-            document.querySelectorAll('#darkmode-css').forEach(el => {
-                el.remove();
-            })
-            
-        } else {
-            addStyles('css/wilmonium-dark.css', 'darkmode-css');
-        }
-        localStorage.setItem('wilmonium', JSON.stringify({darkmode: darkmode}));
-    }
-    //document.getElementsByTagName("footer")[0].children[0].src = "https://media.discordapp.net/attachments/798799175072219136/798840488819294238/treegif.gif";
-}
+        document.querySelector('.toggle-checkbox').onchange = (evt) => {
+            let darkmode = evt.target.checked;
+            wilmoniumDebug('Dark mode ' + darkmode)
+            if (!darkmode) {
+                document.querySelectorAll('#darkmode-css').forEach(el => {
+                    el.remove();
+                })
 
-/* Dark mode for iframes in the message pages */
-document.querySelectorAll('iframe').forEach(iframe => {
-    iframe.setAttribute('allowTransparency', 'true')
-    var style = document.createElement('style');
-    style.innerHTML =
-    `html,body {background-color: transparent !important; }
+            } else {
+                addStyles('css/wilmonium-dark.css', 'darkmode-css');
+            }
+            localStorage.setItem('wilmonium', JSON.stringify({darkmode: darkmode}));
+        }
+        //document.getElementsByTagName("footer")[0].children[0].src = "https://media.discordapp.net/attachments/798799175072219136/798840488819294238/treegif.gif";
+    }
+    /* Dark mode for iframes in the message pages */
+    document.querySelectorAll('iframe').forEach(iframe => {
+        iframe.setAttribute('allowTransparency', 'true')
+        var style = document.createElement('style');
+        style.innerHTML =
+            `html,body {background-color: transparent !important; }
     p,ul,li,b,strong,i,h1,h2,h3,h4,h5,blockquote {color: #bbb;}
     a, a:link, a:visited {color: #3eb0ef !important;}`;
-    iframe.contentWindow.document.body.appendChild(style);
-});
+        iframe.contentWindow.document.body.appendChild(style);
+    });
+
+} catch (e) {
+    wilmoniumDebug(e);
+}
