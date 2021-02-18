eda9f9ce1551039bcb50c106e60d29c38645ce48
developerfromjokela <developerfromjokela@gmail.com>
2021-02-18 14:49:57 +0200

    Dark theme fixes for all wilma pages, including sign in pages

--- js/loader.js
+++ js/loader.js
@@ -48,7 +48,7 @@ const iframePatch = () => {
 };
 
 try {
-    let navDetect = document.getElementsByClassName("nav-wilma").length > 0;
+    let navDetect = document.getElementsByClassName("nav-wilma").length > 0 || document.getElementsByClassName("login-page").length > 0;
     let selectionDetect = document.getElementsByClassName("selection-body").length > 0;
     // Inject code to wilma page, if it happens to be logged in
     if ( navDetect || selectionDetect) {
